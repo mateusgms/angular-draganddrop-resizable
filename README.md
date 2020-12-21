a7a6dc34b0e3efd8eceab49c85c6fd7ca12befc0
mateusgms <mtsgomesp@gmail.com>
2020-12-21 16:54:24 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-vrhlng
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-vrhlng)
---
+++ src/app/cdk-drag-drop-boundary-example.ts
@@ -0,0 +1,16 @@
+import {Component} from '@angular/core';
+
+/**
+ * @title Drag&Drop boundary
+ */
+@Component({
+  selector: 'cdk-drag-drop-boundary-example',
+  templateUrl: 'cdk-drag-drop-boundary-example.html',
+  styleUrls: ['cdk-drag-drop-boundary-example.css'],
+})
+export class CdkDragDropBoundaryExample {}
+
+
+/**  Copyright 2020 Google LLC. All Rights Reserved.
+    Use of this source code is governed by an MIT-style license that
+    can be found in the LICENSE file at http://angular.io/license */
